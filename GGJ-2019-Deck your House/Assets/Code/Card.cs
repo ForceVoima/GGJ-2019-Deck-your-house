1e2dcdcf80ac1b31335168646241c91f5b02a66d
Mikko Voima <voima.mikko@gmail.com>
2019-01-25 21:02:27 +0200

    TextMeshPro & early world space UI testing

---
+++ GGJ-2019-Deck your House/Assets/Code/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [Tooltip("Player 1 joy rating."), Range(-10, 10)]
+    public int player1Rating = 0;
+
+    [Tooltip("Player 2 joy rating."), Range(-10, 10)]
+    public int player2Rating = 0;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
